15842e583f1f60a74d3d34fe4d25657bf0449456
Gary Clayburg <gclaybur@comcast.net>
2018-03-10 15:52:54 -0700

    do not fail on app startup if running with springboot 2.0
    (cherry picked from commit 272d898546e965e8af2825d1d6ff4c197f8e683d)

---
+++ src/main/java/com/garyclayburg/upbanner/WebServerInitialized.java
@@ -0,0 +1,20 @@
+package com.garyclayburg.upbanner;
+/*
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WebServerInitialized implements ApplicationListener<ServletWebServerInitializedEvent> {
+
+    @SuppressWarnings("UnusedDeclaration")
+    private static final Logger log = LoggerFactory.getLogger(WebServerInitialized.class);
+
+    @Override
+    public void onApplicationEvent(ServletWebServerInitializedEvent event) {
+
+    }
+}
+*/
