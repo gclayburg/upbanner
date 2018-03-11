1068ec41aebe4fd8b7505dc9aebeed1a60db7195
Gary Clayburg <gclaybur@comcast.net>
2018-03-11 15:10:29 -0600

    WarningVersion prints when running under Spring Boot 1.x

--- src/main/java/com/garyclayburg/upbanner/WarningVersion.java
+++ src/main/java/com/garyclayburg/upbanner/WarningVersion.java
@@ -12,13 +12,19 @@ import org.springframework.stereotype.Component;
  * @author Gary Clayburg
  */
 @Component
-@ConditionalOnMissingClass({"org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent"})
+@ConditionalOnMissingClass({"org.springframework.boot.web.servlet.context.ServletWebServerInitializedEvent"})
 public class WarningVersion {
 
     @SuppressWarnings("UnusedDeclaration")
     private static final Logger log = LoggerFactory.getLogger(WarningVersion.class);
 
     public WarningVersion() {
-        log.warn("This version of WhatsUp only works with Spring Boot 1.x.  This app uses Spring Boot 2.0+.  Please upgrade WhatsUp to version 2.0.0+.");
+        log.warn("This version of WhatsUp only works with Spring Boot 2.x.  This app uses Spring Boot 1.0+.  Please change WhatsUp to version 1.0.0.");
+        /* won't even be visible unless someone disables the nested dependency like this - but hey, pigs could fly.
+            compile(group: 'com.garyclayburg', name:'upbanner', version: '2.0.0'){
+              exclude(group: 'org.springframework.boot', module: 'spring-boot-starter-web')
+            }
+         */
+
     }
 }
