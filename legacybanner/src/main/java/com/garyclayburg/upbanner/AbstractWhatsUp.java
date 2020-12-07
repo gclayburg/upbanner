62c6a6571e6aa489fa6cbaf4ac31aebe79201aff
Gary Clayburg <gclaybur@comcast.net>
2020-12-07 11:09:39 -0700

    show JVM heap size, OS memory, and cgroups memory limitations
    This tries to help with "How much memory is our app working with?"

--- legacybanner/src/main/java/com/garyclayburg/upbanner/AbstractWhatsUp.java
+++ legacybanner/src/main/java/com/garyclayburg/upbanner/AbstractWhatsUp.java
@@ -1,9 +1,11 @@
 package com.garyclayburg.upbanner;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.lang.management.ManagementFactory;
+import java.nio.file.Paths;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,6 +20,8 @@ import org.springframework.core.env.Environment;
  */
 public abstract class AbstractWhatsUp {
 
+    public static final String MEM_TOTAL_REGEX = "^MemTotal:\\s*(\\d+)\\s*kB.*$";
+    public static final Pattern MEM_TOTAL_REGEX_PATTERN = Pattern.compile(MEM_TOTAL_REGEX);
     protected Environment environment;
     protected BuildProperties buildProperties;
 
@@ -96,11 +100,64 @@ public abstract class AbstractWhatsUp {
         dumpBuildProperties();
         dumpGitProperties();
         dumpJVMargs();
+        dumpMemory();
+    }
+
+    protected void dumpMemory() {
+        String message = String.format("JVM heap free memory:  %15s (%sm)", Runtime.getRuntime().freeMemory(), Runtime.getRuntime().freeMemory() / 1024 / 1024);
+        log.info(message);
+        log.info(String.format("JVM heap total memory: %15s (%sm)", Runtime.getRuntime().totalMemory(), Runtime.getRuntime().totalMemory() / 1024 / 1024));
+        log.info(String.format("JVM heap max memory:   %15s (%sm)", Runtime.getRuntime().maxMemory(), Runtime.getRuntime().maxMemory() / 1024 / 1024));
+        long jvmMaxMemoryKb = Runtime.getRuntime().maxMemory() / 1024;
+        File file = Paths.get("/sys/fs/cgroup/memory/memory.limit_in_bytes").toFile();
+        String memoryLimitInBytes = "unknown";
+        String memoryLimitMB = "unknown";
+        long cgroupsMemLimitKb = Long.MAX_VALUE;
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            if (file.canRead()) {
+
+                memoryLimitInBytes = br.readLine();
+                long memLimitMB = Long.parseLong(memoryLimitInBytes) / 1024 / 1024;
+                cgroupsMemLimitKb = Long.parseLong(memoryLimitInBytes) / 1024;
+                memoryLimitMB = String.valueOf(memLimitMB);
+            }
+        } catch (IOException ignored) {
+        }
+        File procMeminfo = new File("/proc/meminfo");
+        long memTotalKbLong = Long.MAX_VALUE;
+        try (BufferedReader br = new BufferedReader(new FileReader(procMeminfo))) {
+            if (procMeminfo.canRead()) {
+                String memTotalKb = br.lines()
+                        .filter(line -> line.matches(MEM_TOTAL_REGEX))
+                        .map(line -> {
+                            Matcher m = MEM_TOTAL_REGEX_PATTERN.matcher(line);
+                            String memKb = "";
+                            if (m.find()) {
+                                memKb = m.group(1);
+                            }
+                            return memKb;
+                        })
+                        .reduce("", (a, b) -> b);
+                memTotalKbLong = Long.parseLong(memTotalKb);
+                log.info(String.format("OS Total Memory avail: %15s (%sm)", memTotalKbLong * 1024, memTotalKbLong / 1024));
+            }
+        } catch (IOException ignored) {
+        }
+        if (cgroupsMemLimitKb >= memTotalKbLong) { //no mem limit imposed on our cgroup
+            log.info("cgroups memory limit:        unlimited");
+        } else { // our container was started with limited memory
+            if (cgroupsMemLimitKb < jvmMaxMemoryKb) {
+                log.warn(String.format("cgroups memory limit:  %15s (%sm)", memoryLimitInBytes, memoryLimitMB));
+                log.warn("JVM max heap size is too big for our cgroup." + System.lineSeparator() + "  This JVM will likely become unstable under memory pressure, i.e.," + System.lineSeparator() + "  the kernel could rudely kill the JVM if it requests too much memory. ");
+            } else {
+                log.info(String.format("cgroups memory limit:  %15s (%sm)", memoryLimitInBytes, memoryLimitMB));
+            }
+        }
     }
 
     protected void dumpBuildProperties() {
         log.info("  build properties");
-        buildProperties.forEach(prop -> log.info("buildprop "+ prop.getKey() +": "+prop.getValue()));
+        buildProperties.forEach(prop -> log.info("buildprop " + prop.getKey() + ": " + prop.getValue()));
     }
 
     protected void dumpGitProperties() {
