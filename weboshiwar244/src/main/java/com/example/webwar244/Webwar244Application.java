3b78bfb8f69ace56544ef9d1537e68e9e1696b7e
Gary Clayburg <gclaybur@comcast.net>
2021-03-23 10:18:28 -0600

    added tests for war,oshiprobe, custom name

---
+++ weboshiwar244/src/main/java/com/example/webwar244/Webwar244Application.java
@@ -0,0 +1,13 @@
+package com.example.webwar244;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webwar244Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webwar244Application.class, args);
+	}
+
+}
---
+++ weboshiwar244/src/test/java/com/example/webwar244/Webwar244ApplicationTests.java
@@ -0,0 +1,52 @@
+package com.example.webwar244;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"upbanner.debug=true"})
+class Webwar244ApplicationTests {
+
+    private static final Logger log = LoggerFactory.getLogger(Webwar244ApplicationTests.class);
+
+    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private static final PrintStream originalOutputStream = System.out;
+    private static boolean capturingOutput = false;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        log.debug("running Webwar244ApplicationTests beforeAll");
+        System.setOut(new PrintStream(outContent));
+        capturingOutput = true;
+    }
+
+    @BeforeEach
+    void setUp() {
+        log.debug("running setup()");
+        if (capturingOutput) {
+            System.setOut(originalOutputStream); // only capture initial server start
+            capturingOutput = false;
+            log.debug("captured output follows: ");
+            System.out.println(outContent);
+        }
+    }
+
+    @Test
+    void upbannerisupwithoshiprobe() {
+        assertTrue(outContent.toString().contains("CustomAppNameHere is UP!"));
+        assertTrue(outContent.toString().contains("Host System"));
+        assertTrue(outContent.toString().contains("ProcessorID"));
+    }
+}
