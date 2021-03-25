1c85e75fe9585199e22ee94f47197b902cf20b9a
Gary Clayburg <gclaybur@comcast.net>
2021-03-25 00:50:51 -0600

    files for jaronly15 module

---
+++ jaronly15/src/main/java/com/example/demo/WebJar1519Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJar1519Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJar1519Application.class, args);
+	}
+
+}
---
+++ jaronly15/src/test/java/com/example/demo/JarOnly15applicationTests.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {WebJar1519Application.class})
+public class JarOnly15applicationTests {
+
+    public static final Logger log = LoggerFactory.getLogger(JarOnly15applicationTests.class);
+    private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private static final PrintStream originalOutputStream = System.out;
+    private static boolean capturingOutput = false;
+
+    public JarOnly15applicationTests() {
+    }
+
+    @BeforeClass
+    public static void beforeClass() {
+        log.debug("running Jar1519ApplicationTests beforeClass");
+        System.setOut(new PrintStream(outContent));
+        capturingOutput = true;
+    }
+
+    //autowire simple bean
+    @Autowired
+    FooService fooService;
+
+    @Rule
+    public TestName testName = new TestName();
+
+    @Before
+    public void setUp() {
+        log.info("settingup");
+        log.debug("Running test setUp: " + testName.getMethodName());
+        if (capturingOutput) {
+            System.setOut(originalOutputStream); // only capture initial server start
+            capturingOutput = false;
+            log.debug("captured output follows: ");
+            log.info("showing output here");
+            System.out.println(outContent);
+        }
+    }
+
+    @Test
+    public void upbannerApplicationIsUP() {
+        assertTrue(outContent.toString().contains("Started "));
+        log.info("log me please");
+    }
+}
