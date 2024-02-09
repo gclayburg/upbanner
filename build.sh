54107271af8448aa2809328162cd4dc0389efcb1
Gary Clayburg <gclaybur@comcast.net>
2024-02-08 17:24:16 -0700

    build testing

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 java -version
-export "SDKMAN_DIR=$HOME/.sdkman/"
-source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+if [ -f "$HOME/.sdkman" ]; then  #standard SDK install location
+	export SDKMAN_DIR="$HOME/.sdkman"
+elif [ -f /usr/local/sdk/.sdkman ]; then  #jenkins build agent uses this
+	export SDKMAN_DIR=/usr/local/sdk/.sdkman
+fi
+source "$SDKMAN_DIR/bin/sdkmain-init.sh"
+echo "now using sdkman I hope"
+java -version
 #sdk list java
 sdk use java 17.0.10-tem
 java -version
