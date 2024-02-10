5f74e11785b153cf9759dcf5abb337ccfe5aa580
Gary Clayburg <gclaybur@comcast.net>
2024-02-09 22:09:47 -0700

    disable BuildPropertiesStub

--- upbanner-starter/src/main/java/com/garyclayburg/upbanner/BuildPropertiesStub.java
+++ upbanner-starter/src/main/java/com/garyclayburg/upbanner/BuildPropertiesStub.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
  * Creates a minimal {@link BuildProperties}  with {@link BuildPropertiesStub#buildProperties}
  * @author Gary Clayburg
  */
-@Configuration
+//@Configuration
 public class BuildPropertiesStub {
 
     @SuppressWarnings("UnusedDeclaration")
@@ -25,8 +25,8 @@ public class BuildPropertiesStub {
      *     If {@code boot-info.properties} <b>is</b> on the classpath, then we expect Spring Boot will create a fully populated {@link BuildProperties} bean
      * @return minimal {@link BuildProperties}
      */
-    @Conditional(ConditionalOnMissingBuildInfo.class)
-    @Bean
+//    @Conditional(ConditionalOnMissingBuildInfo.class)
+//    @Bean
     public BuildProperties buildProperties() {
         Properties p = new Properties();
         p.put("org.label-schema.build-date", "development");
