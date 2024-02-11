3969547902f6234fd2686ab72ae802b0d0e45fb3
Gary Clayburg <gclaybur@comcast.net>
2024-02-11 12:12:05 -0700

    better fullbuild.sh error detection and  reporting

--- fullbuild.sh
+++ fullbuild.sh
@@ -8,6 +8,10 @@ source "$SDKMAN_DIR/bin/sdkman-init.sh"
 
 sdk use java 17.0.10-tem
 mvn clean javadoc:jar source:jar install
+exitcode=$?
+if [ $exitcode -ne 0 ]; then
+  exit $exitcode
+fi
 sdk use java 8.0.402-tem
 mvn install
 
